41785eed678b02d1e2bf5abfeb5b6bccaf27d28c
Abdush <abdushs54@gmail.com>
2021-11-27 16:59:08 +0530

    added routes part

---
+++ src/Hoc/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const index = () => {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <Footer />
+    </div>
+  );
+};
+
+export default index;
